608016541ae1ace37e4ae940c12c628e1e72680d
papihoudini <ochiengcliff.co@gmail.com>
2023-09-28 03:22:28 -0400

    Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,20 +1,33 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignUp(firstName, lastName, fileName) {
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
+async function handleProfileSignup(firstName, lastName, fileName) {
+  const promise1 = {
+    status: 'pending ',
+  };
+  const promise2 = {
+    status: 'pending ',
+  };
 
-  const [userResult, photoResult] = await Promise.allSettled([userPromise, photoPromise]);
+  try {
+    const signup = await signUpUser(firstName, lastName);
+    promise1.status = 'fulfilled';
+    promise1.value = signup;
+  } catch (err) {
+    promise1.status = 'rejected';
+    promise1.value = err.toString();
+  }
 
-  return [
-    {
-      status: userResult.status,
-      value: userResult.status === 'fulfilled' ? userResult.value : userResult.reason,
-    },
-    {
-      status: photoResult.status,
-      value: photoResult.status === 'fulfilled' ? photoResult.value : photoResult.reason,
-    },
-  ];
+  try {
+    const upload = await uploadPhoto(fileName);
+    promise2.status = 'fulfilled';
+    promise2.value = upload;
+  } catch (err) {
+    promise2.status = 'rejected';
+    promise2.value = err.toString();
+  }
+
+  return [promise1, promise2];
 }
+
+export default handleProfileSignup;

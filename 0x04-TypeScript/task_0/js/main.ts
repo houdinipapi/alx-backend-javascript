bb935c93dab9ef0ac5b43467400a4b974f3c0187
papihoudini <ochiengcliff.co@gmail.com>
2023-10-04 17:15:52 -0400

    Task_0

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,46 @@
+// Define the Student interface
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+// Create two students
+const student1: Student = {
+  firstName: 'John',
+  lastName: 'Doe',
+  age: 20,
+  location: 'New York',
+};
+
+const student2: Student = {
+  firstName: 'Alice',
+  lastName: 'Smith',
+  age: 22,
+  location: 'Los Angeles',
+};
+
+// Create an array of students
+const studentsList: Student[] = [student1, student2];
+
+// Render a table with student information
+const table = document.createElement('table');
+const tbody = document.createElement('tbody');
+
+studentsList.forEach((student) => {
+  const row = document.createElement('tr');
+  const firstNameCell = document.createElement('td');
+  const locationCell = document.createElement('td');
+
+  firstNameCell.textContent = student.firstName;
+  locationCell.textContent = student.location;
+
+  row.appendChild(firstNameCell);
+  row.appendChild(locationCell);
+
+  tbody.appendChild(row);
+});
+
+table.appendChild(tbody);
+document.body.appendChild(table);
